ff24cf717aa9e11cbab7922439355c7801be8131
QQbar2013 <jacky999200@gmail.com>
2025-10-08 18:48:26 +0800

    Update script.js

--- pickupless/script.js
+++ pickupless/script.js
@@ -99,7 +99,6 @@ const locationConfig = {
       dates: [],
       ranges: [
         { start: "2025-10-08", end: "2025-10-29" },
-        { start: "2025-10-31", end: "2025-11-05" },
         { start: "2025-11-07", end: "2099-05-24" }
       ]
     }
